fc87e034ba22f4ae44d10d09ffb0100bbdbd0f2b
anilkumarmeena <anil98meena@gmail.com>
2020-03-28 16:16:41 +0530

    homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,257 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final List<String> imgList = [
+    'https://houseofclashers.com/r/clash-of-clans/images/resize/2020-03-super-troops.547x300q50.jpg',
+    'https://houseofclashers.com/r/clash-of-clans/images/resize/2020-03-super-giant-00.533x300q50.jpg',
+    'https://houseofclashers.com/r/clash-of-clans/images/resize/2020-03-sneaky-goblin-01.533x300q50.jpg',
+    'https://houseofclashers.com/r/clash-of-clans/images/resize/super-barbarian-02.533x300q50.jpg',
+    'https://houseofclashers.com/r/clash-of-clans/images/resize/sneak_peeks.549x300q50.jpg',
+    'https://houseofclashers.com/r/clash-of-clans/images/resize/clangames.574x300q50.jpg'
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final CarouselSlider updates = CarouselSlider(
+      viewportFraction: 0.9,
+      aspectRatio: 2.0,
+      autoPlay: true,
+      enlargeCenterPage: true,
+      items: imgList.map(
+        (url) {
+          return Container(
+            margin: EdgeInsets.all(5.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(5.0)),
+              child: Image.network(
+                url,
+                fit: BoxFit.cover,
+                width: double.maxFinite,
+              ),
+            ),
+          );
+        },
+      ).toList(),
+    );
     return Scaffold(
+      backgroundColor: Colors.black,
+      drawer: nDrawer(),
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        backgroundColor: Colors.black,
+        title: Text(
+          "Guide of COC",
+          style:
+              TextStyle(fontWeight: FontWeight.w800, color: Colors.deepPurple),
         ),
+        actions: <Widget>[
+          GestureDetector(
+            onTap: () {},
+            child: Icon(
+              Icons.more_vert,
+              color: Colors.deepPurpleAccent,
+            ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          updates,
+          SizedBox(
+            height: 50,
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Home Village",
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w800,
+                      color: Colors.deepPurple),
+                  textAlign: TextAlign.left,
+                ),
+                Container(
+                  height: 150,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: new List.generate(
+                        imgList.length,
+                        (index) => Container(
+                            margin: EdgeInsets.all(4.0),
+                            child: Stack(
+                              children: <Widget>[
+                                Container(
+                                  child: Image.network(
+                                    imgList[index],
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                Positioned(
+                                    bottom: 0,
+                                    child: Container(
+                                      width: 300,
+                                      color: Colors.black.withOpacity(0.5),
+                                      child: Center(
+                                        child: Text("Attack strategy",
+                                            style: TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.w400,
+                                                color: Colors.white)),
+                                      ),
+                                    )),
+                              ],
+                            ))),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 30),
+            padding: EdgeInsets.only(left: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Builder Village",
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w800,
+                      color: Colors.deepPurple),
+                  textAlign: TextAlign.left,
+                ),
+                Container(
+                  height: 150,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: new List.generate(
+                        imgList.length,
+                        (index) => Container(
+                            margin: EdgeInsets.all(4.0),
+                            child: Stack(
+                              children: <Widget>[
+                                Image.network(imgList[index]),
+                                Text("data")
+                              ],
+                            ))),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget nDrawer() {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Text(' '),
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(
+                        "https://houseofclashers.com/r/clash-of-clans/images/resize/2020-03-sneaky-goblin-01.533x300q50.jpg"))),
+          ),
+          Row(
+            children: <Widget>[
+              SizedBox(
+                width: 5,
+              ),
+              Icon(Icons.star_border,color: Colors.redAccent,size: 30,),
+              SizedBox(
+                width: 15,
+              ),
+              Text("Rate Us",
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.black)),
+            ],
+          ),
+          SizedBox(
+                height: 20,
+              ),
+          Row(
+            children: <Widget>[
+              SizedBox(
+                width: 5,
+              ),
+              Icon(Icons.share,color: Colors.purple,size: 30,),
+              SizedBox(
+                width: 15,
+              ),
+              Text("Share",
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.black)),
+            ],
+          ),
+          SizedBox(
+                height: 20,
+              ),
+          Row(
+            children: <Widget>[
+              SizedBox(
+                width: 5,
+              ),
+              Icon(Icons.book,color: Colors.brown,size: 30,),
+              SizedBox(
+                width: 15,
+              ),
+              Text("Privacy & Policy",
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.black)),
+            ],
+          ),
+          SizedBox(
+                height: 20,
+              ),
+          Row(
+            children: <Widget>[
+              SizedBox(
+                width: 5,
+              ),
+              Icon(Icons.mail,color: Colors.pink,size: 30,),
+              SizedBox(
+                width: 15,
+              ),
+              Text("Contact Us",
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.black)),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
