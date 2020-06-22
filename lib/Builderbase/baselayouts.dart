e2ea260ad34b759e2677a970088a68c9799339ed
wrangler8 <sunil98meena@gmail.com>
2020-06-22 23:02:12 +0530

    baselayout screen updated

--- lib/Builderbase/baselayouts.dart
+++ lib/Builderbase/baselayouts.dart
@@ -1,17 +1,54 @@
 import 'package:flutter/material.dart';
-class BuilderBaseBaseLayouts extends StatelessWidget {
+class BuilderBaseBaseLayouts extends StatefulWidget {
+  @override
+  _BuilderBaseBaseLayoutsState createState() => _BuilderBaseBaseLayoutsState();
+}
+
+class _BuilderBaseBaseLayoutsState extends State<BuilderBaseBaseLayouts> {
+  final List<Tab> myTabs = <Tab>[
+    Tab(text: 'LEFT'),
+    Tab(text: 'RIGHT'),
+    Tab(text: "Bottom"),
+    Tab(text: "Up",),
+  ];
+  
   @override
   Widget build(BuildContext context) {
     final title = 'Base Layouts';
     
 
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
+    return
+    DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Text("WAR")),
+                Tab(icon: Text("TROPHY")),
+                Tab(icon: Text("HYBRID")),
+                Tab(icon: Text("FARMING")),
+              ],
+            ),
+            title: Text(title),
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              baseList("WAR"),
+              baseList("TROPHY"),
+              baseList("HYBRID"),
+              baseList("FARMING"),
+            ]
+          ),
         
-      );
+      ),
+    );
+    
+  }
+  Container baseList(String type) {
+    return Container(
+      child: Text(type),
+    );
     
   }
 }
-
